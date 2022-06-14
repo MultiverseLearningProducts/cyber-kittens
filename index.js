1e96f69e08af8c3c95c370b83356552d5184122b
Preston Wallace <wallace.preston@gmail.com>
2022-06-14 08:04:41 -0700

    feat(errors): Error handler

--- index.js
+++ index.js
@@ -34,5 +34,12 @@ app.get('/', async (req, res, next) => {
 // DELETE /cats/:id
 // TODO - takes an id and deletes the cat with that id
 
+// error handling middleware, so failed tests receive them
+app.use((error, req, res, next) => {
+  console.error('SERVER ERROR: ', error);
+  if(res.statusCode < 400) res.status(500);
+  res.send({error: error.message, name: error.name, message: error.message});
+});
+
 // we export the app, not listening in here, so that we can run tests
 module.exports = app;
